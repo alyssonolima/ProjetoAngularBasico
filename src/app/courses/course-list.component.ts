63269e93564e7527a2821e3ad8bbdba4a5359083
Alysson Lima <alysson_lima@bct.ect.ufrn.br>
2020-09-17 01:13:34 -0300

    Aula dia 2
    Injeção de dependencias;
    Data binding;
    Pipes e
    Rotas

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,39 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Template } from '@angular/compiler/src/render3/r3_ast';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit{
-    courses: Course[] = [];
+    filteredCourses: Course[] = [];
+
+    _courses: Course[] = [];
+    
+    _filterBy: string;
+
+    constructor(private courseService: CourseService){  }
 
     ngOnInit(): void{
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular Curso',
-                imageurl: 'assets/images/forms.png',
-                price: 99.99,
-                code: 'xps-100',
-                duration: 128,
-                rating: 8.3,
-                releaseDate: 'Maio. 5. 1989'
-            
-            },
-            {
-                id: 2,
-                name: 'Angular Curso parte 2',
-                imageurl: 'assets/images/http.png',
-                price: 299.99,
-                code: 'xps-200',
-                duration: 500,
-                rating: 9.3,
-                releaseDate: 'Setembro. 16. 2020'
-            
-            }
-        ]
+        this._courses = this.courseService.retrievAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string){
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1 );
+    }
+
+    get filter(){
+        return this._filterBy;
     }
 
 }
--- src/app/courses/course.ts
+++ src/app/courses/course.ts
@@ -1,10 +1,11 @@
 export class Course{
     id: number;
     name: string;
-    imageurl: string;
+    imageUrl: string;
     price: number;
     code: string;
     duration: number;
     rating: number;
-    releaseDate: string
+    releaseDate: string;
+    description: string
 }
